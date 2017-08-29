3c6ba20b12269678f9e95e67ce406dec10084564
rockGithub <rock20110414@gmail.com>
2017-08-29 20:25:17 +0800

    first step, ready for milestone 01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterComponentComponent } from './router-component/router-component.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
 
-
+const appRoutes: Routes = [
+  { path: 'crisis-center', component: CrisisListComponent },
+  { path: 'hero/:id',      component: HeroDetailComponent },
+  {
+    path: 'heroes',
+    component: HeroListComponent,
+    data: { title: 'Heroes List' }
+  },
+  { path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RouterComponentComponent
+    RouterComponentComponent,
+    HeroDetailComponent,
+    CrisisListComponent,
+    PageNotFoundComponent,
+    HeroListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
